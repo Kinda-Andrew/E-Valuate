ecf21853ba9f7cd83e8129a8d00f88690f41b8ce
Augus Li <augus1101@gmail.com>
2025-10-04 18:39:11 -0700

    fuck !

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,16 +7,39 @@ function Home(){
 
 return(
     <>
-    <div className = "titlecontainer">
-    <div className = "headervideo">
-    <video>
+   <div className = "video"> 
+        <video autoPlay muted loop id = "video">
+        <source src = "/rain.mp4" type = "video/mp4"/>
+        </video>
+    </div>
     
-     <source src = "https://raw.githubusercontent.com/Kinda-Andrew/E-Valuate/refs/heads/b%2Batest/rain.mp4" type="video/mp4" alt = "video not supported"/>
-     
-    </video> </div>
 
+
+    <div className = "titlecontainer">
+      
+
+    
 
     <div className = "title"> Evaluate </div>
+      
+    </div>
+
+    <div className = "lowerpart">
+
+    
+        <div className = "leftside">
+            This is the left side
+
+        
+        
+
+        </div>
+
+        <div className = "rightside">
+            This is the right side
+            
+        
+        </div>
 
     </div>
 
