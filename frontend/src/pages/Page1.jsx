c0862132e2d6928de5ca456558e8e127374ea2c8
brodrigues2005 <rodriguesbrett85@gmail.com>
2025-10-05 07:05:28 -0700

    end point fully connected and working

--- frontend/src/pages/Page1.jsx
+++ frontend/src/pages/Page1.jsx
@@ -1,52 +1,61 @@
 import React from "react";
-import './Page1.css'
+import "./Page1.css";
 import { useState } from "react";
 
+function Page1() {
+  const [inputFile, setInputFile] = useState(null);
+  const [outputFile, setOutputFile] = useState(null);
 
-
-function Page1(){
-     const[inputFile,setInputFile] = useState(null);
-     const[ouputFile,setOutputFile] = useState(null);
-
-     async function handleSubmit(event){
+  async function handleSubmit(event) {
     event.preventDefault();
 
     const formData = new FormData();
     formData.append("file", inputFile);
 
-    const res = await fetch(`${process.env.VITE_API_URL}/getPhoto`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: formData,});
+    console.log([...formData.entries()]);
 
-    console.log(res);
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/getPhoto`, {
+      method: "POST",
+      body: formData,
+    });
 
-}
-    
+    const blob = await res.blob(); // <-- instead of .json()
+    const imageUrl = URL.createObjectURL(blob);
+    console.log(imageUrl);
+    setOutputFile(imageUrl);
+  }
 
-    return(
-        
-        
-        <>
+  if (!outputFile) {
+    return (
+      <>
         <form onSubmit={handleSubmit}>
-            <input type="file" onChange={(event)=>{setInputFile(event.target.files[0]);}}></input>
-            <button type="submit">Submit</button>
+          <input
+            type="file"
+            onChange={(event) => {
+              setInputFile(event.target.files[0]);
+            }}
+          ></input>
+          <button type="submit">Submit</button>
         </form>
-
-        </>
-
+      </>
     );
+  }
 
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="file"
+          onChange={(event) => {
+            setInputFile(event.target.files[0]);
+          }}
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
 
-   
-        <h1>{inputFile.name}</h1>
-
-
-
-
+      <img src={outputFile} style={{ height: "80vh" }} alt="Output Image" />
+    </>
+  );
 }
-
 
 export default Page1;
